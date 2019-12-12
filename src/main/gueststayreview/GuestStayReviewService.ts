1ef3321eb625e167d9158deb1961fc03d1f642ef
kamilkamil95 <kamilhnat@gmail.com>
2019-12-12 12:55:13 +0100

    Express your opinion, mail to customer

---
+++ src/main/gueststayreview/GuestStayReviewService.ts
@@ -0,0 +1,16 @@
+import {CommandResult} from "../sharedkernel/application/CommandResult";
+import {SendEmailCommand} from "./emailmessage/SendEmailCommand";
+
+export interface EmailSender{
+    execute(command: SendEmailCommand): Promise<CommandResult>
+}
+
+function sendforReview(emailSender:EmailSender){
+
+emailSender.execute(command:{
+    recipient:`${recipient}`,
+    subject:'express you opinion',
+    content:`please express you opinion and click below ${}`
+})
+
+}
