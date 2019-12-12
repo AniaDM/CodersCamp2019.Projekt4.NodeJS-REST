f8f78d97035510a12f43284fd9216076da4f77cd
kamilkamil95 <kamilhnat@gmail.com>
2019-12-12 18:23:01 +0100

    Rated rooms filter

---
+++ src/main/gueststayreview/ratedRooms.ts
@@ -0,0 +1,31 @@
+import {RoomOfferRepository} from "./RoomOfferRepository";
+import {RoomOffer} from "./RoomOffer";
+
+export class RoomSearcher {
+
+    constructor(private roomOfferRepository: RoomOfferRepository) {
+    }
+
+    async searchOffersBy(filter: OfferFilter) {
+        return this.roomOfferRepository.getAll()
+            .then(offers => this.filterOffersBy(offers, filter));
+    }
+
+     filterOffersBy(offers: RoomOffer[], filter: OfferFilter): RoomOffer[] {
+        return offers.filter(filter.averageRating > 0)
+        ||
+        offers.filter(filter.averageRating == 0 || filter.averageRating == undefined || filter.averageRating == null );
+    }
+
+   
+
+}
+
+export interface OfferFilter {
+    numberOfGuests?: number,
+    minPrice?: number,
+    maxPrice?: number,
+    minAverageRating?: number,
+    location?: string,
+    averageRating?:number
+}
