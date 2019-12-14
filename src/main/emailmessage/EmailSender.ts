19296753726b8d9b433da413f408d6208b8b5245
Piotr <bozicss@gazeta.pl>
2019-12-14 10:30:47 +0100

    correcting email sending

--- src/main/emailmessage/EmailSender.ts
+++ src/main/emailmessage/EmailSender.ts
@@ -1,32 +1,48 @@
 import {CommandResult} from "../sharedkernel/application/CommandResult";
+import { SendEmailCommand } from "./SendEmailCommand";
 
 export interface EmailSender {
 
     execute(command: SendEmailCommand): Promise<CommandResult>
-
 }
-const textMail='We confirm your reservation: '; //potwierdzenie rezerwacji
+
 const nodemailer = require ('nodemailer')
+ 
+class MailGunEmailSender implements EmailSender {
 
-//u Kamila to samo!!!!
-let transporter = nodemailer.createTransport({
-    auth: {
-        api_key: '3b131fb4448f106936702baa0784a2ad-5645b1f9-efed18c9',
-        domain: 'sandbox431c4ae01721472484bbb0d86cbfb6c2.mailgun.org'
-    }
-});
+  execute( command: SendEmailCommand: Promise<CommandResult>) {
+    
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+       auth: {
+        user: 'bozikiart@gmail.com',
+        pass: 'string' }
+        })
+    
+    var mailOptions = {
+          from :  'Room Owner <me@CodersCamp.pl>',
+          to : 'boziki@gazeta.pl', 
+          subject : 'Hello - re-email', 
+          text: 'text text text' 
+    }; 
+        
+    transporter.sendMail( mailOptions, (error, info) => { 
+          if (error) { 
+            return console.log(`error: ${error}`); 
+          } 
+          console.log(`Message Sent ${info.response}`); 
+        }); 
+}
 
-let mailOptionReservation = {
-    from: 'me@CodersCamp.pl',
-    to: 'info@boziki.pl', // dla testu Ma byc mail klienta wyszkukany po name lub ID
-    subject: 'Reservation confirmation',
-    text: textMail // dodatkowe dane odnośnie klienta: termin rezerwacji, ilość osób i pokoi, opcje dodatkowe.
-};
 
-transporter.sendMail(mailOptionReservation, function(err,data){
-    if(err) {
-        console.log('Err is error', err);
-    } else {
-        console.log('Email ok!');
-    }
-})t
+ class ConsoleLogEmailSender implements EmailSender{
+
+   execute(command: SendMailCommans): Promise<CommandResult> {
+    
+    console.log('Email logged in console', command);
+    
+    return Promise.resolve(CommandResult.success);
+   }
+ }
+
+
