f5db1b72352fcb1f5c920d20f9d31e1099266942
Piotr <bozicss@gazeta.pl>
2019-12-22 19:03:13 +0100

    Częściowa redukcja błędów

--- src/main/emailmessage/EmailSender.ts
+++ src/main/emailmessage/EmailSender.ts
@@ -1,47 +1,47 @@
 require('dotenv').config();
-import {CommandResult} from "../sharedkernel/application/CommandResult";
+import { CommandResult } from "../sharedkernel/application/CommandResult";
 import { SendEmailCommand } from "./SendEmailCommand";
 import { string } from "joi";
 
 export interface EmailSender {
 
-    execute(command: SendEmailCommand): Promise<CommandResult>
+  execute(command: SendEmailCommand): Promise < CommandResult >
 }
 
-const nodemailer = require ('nodemailer')
- 
+const nodemailer = require('nodemailer')
+
 class MailGunEmailSender implements EmailSender {
 
-  execute(command: SendEmailCommand: Promise<CommandResult>) {
-    
-    let transporter = nodemailer.createTransport({
-      service:  'gmail',
-       auth: {
-        user:  process.env.EMAIL,
-        pass:  process.env.PASSWORD}
-        })
-    
-    let mailOptions = {
-          from : process.env.EMAIL,
-          to : SendEmailCommand.recipient, 
-          subject : SendEmailCommand.subject, 
-          text: SendEmailCommand.content 
-    }; 
-        
-    transporter.sendMail( mailOptions, (error, info) => { 
-          if (error) { 
-            return console.log(`error: ${error}`); 
-          } 
-          console.log(`Message Sent ${info.response}`); 
-        }); 
-}
+  execute(command: SendEmailCommand): Promise < CommandResult >    {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+      }
+    })
 
- class ConsoleLogEmailSender implements EmailSender{
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: SendEmailCommand.recipient,
+      subject: SendEmailCommand.subject,
+      text: SendEmailCommand.content
+    };
 
-   execute(command: SendMailCommans): Promise<CommandResult> {
-    
-    console.log('Email logged in console', command);
-    
-    return Promise.resolve(CommandResult.success);
-   }
- }
+    transporter.sendMail(mailOptions, (error: any, info: any) => {
+      if (error) {
+        return console.log(`error: ${error}`);
+      }
+      console.log(`Message Sent ${info.response}`);
+    });
+  }
+
+  class ConsoleLogEmailSender implements EmailSender {
+
+    execute(command: SendEmailCommand): Promise < CommandResult > {
+
+      console.log('Email logged in console', command);
+
+      return Promise.resolve(CommandResult.success);
+    }
+  }
