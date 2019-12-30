320b1341be3459c9c2bc385f2b694e49d3162465
Ewa <ewzdun@gmail.com>
2019-12-30 12:21:41 +0100

    #46 poprawione req.query

--- src/main/restapi/routes/RoomSearchRoutes.ts
+++ src/main/restapi/routes/RoomSearchRoutes.ts
@@ -1,21 +1,25 @@
+import * as express from 'express';
 import {RoomSearcher} from "../../roomsearch/RoomSearcher";
-import {isDefined, isNotDefined} from "../../utils";
-import RestApiException from "../exception/RestApiException";
-import {ErrorCode} from "../../sharedkernel/domain/ErrorCode";
+import {isDefined} from "../../utils";
 
 
 export default (roomSearcher: RoomSearcher) => {
     const router: express.Router = express.Router();
 
-    router.get('/:offers', async (req, res) => {
-        const filter = req.query.filter;
-        const offers = await RoomSearcher.searchOffersBy(filter);
+    router.get(`/offers`, async (req, res) => {
+        
+        const filter = req.query;
+
+        const offers = await roomSearcher.searchOffersBy(filter);
+
         if (isDefined(offers)) {
             res.send(offers)
         } else {
-            next(new RestApiException(404, `Offers for this filter not found!`, ErrorCode.OFFERS_NOT_FOUND))
+            return []
         }
     })
+
+    return router
 }
 
 export const ROUTE_URL = '/offers';
