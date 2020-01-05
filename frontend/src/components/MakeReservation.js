9b0a049ba884191488bd2a97e85ad360f62d61d6
Ewa <57043308+ewzdun@users.noreply.github.com>
2020-01-05 13:46:58 +0100

    #67 Main menu

---
+++ frontend/src/components/MakeReservation.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Menu from './Menu';
+
+const MakeReservation = () => {
+    return <div><Menu />Strona z formularzem rezerwacji pokoju</div>
+};
+
+export default MakeReservation
