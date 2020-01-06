77fa8bb5f3db674adb443032c751de77193b489b
Piotr <boziki@gazeta.pl>
2020-01-06 16:05:19 +0100

    commitPiotrver4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import ListOfPreservedRooms from './components/listOfPreservedRooms';
-import ListOfRoomReviews from './components/listOfRoomReviews';
-// import ScrollableTabsButtonForce from './menu'
-
-import AppForm from './components/AppForm';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Login from './components/Log'
+import Register from './components/Register'
+import RoomOffers from './components/RoomOffers'
+import MakeReservation from './components/MakeReservation'
+import GuestReservations from './components/GuestReservations'
+import AcceptReservations from './components/AcceptReservations'
+import SendOpinion from './components/SendOpinion'
+import UserProfile from './components/UserProfile'
+import AddOffer from './components/AddOffer'
 
 const App = () => {
-  return (
-  <div>
-        {/* <ListOfPreservedRooms /> 
-        <ListOfPreservedRooms />
-        <ListOfPreservedRooms /> */}
-         <ListOfPreservedRooms />  
-         <ListOfRoomReviews />
-        <ListOfRoomReviews />
-        <ListOfRoomReviews />
-        <ListOfPreservedRooms />  
-         <ListOfRoomReviews /> 
-        <AppForm /> 
-      
-  </div>
-
- );
+  return (<div>
+    <BrowserRouter>
+    <div>
+    <Route path="/" exact component={Login} />
+   
+    <Route path="/roomoffers" exact component={RoomOffers} />
+    <Route path="/makereservation" exact component={MakeReservation} />
+    <Route path="/questreservations" exact component={GuestReservations} />
+    <Route path="/acceptreservations" exact component={AcceptReservations} />
+    <Route path="/sendopinion" exact component={SendOpinion} />
+    <Route path="/userprofile" exact component={UserProfile} />
+    <Route path="/addoffer" exact component={AddOffer} />
+    <Route path="/register" exact component={Register} />
+    </div>
+    </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
