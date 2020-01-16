ef56bc36d889ba60dc06b4f03422080bc9555e7a
Piotr <boziki@gazeta.pl>
2020-01-16 15:23:52 +0100

    commit

--- frontend/src/components/Log.js
+++ frontend/src/components/Log.js
@@ -1,7 +1,105 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './loginScreen.css';
+import logo from './test.jpg';
 
-const Login = () => {
-    return <div>Strona z logowaniem</div>
-};
+class LoginPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+      error: '',
+    };
 
-export default Login
+    
+
+
+
+
+    this.handlePassChange = this.handlePassChange.bind(this);
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.dismissError = this.dismissError.bind(this);
+  }
+
+  dismissError() {
+    this.setState({ error: '' });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+  
+    if(this.state.username.length > 0 && this.state.password.length > 0){
+
+
+      console.log(`${document.querySelector('.userNameInputG').value} and ${document.querySelector('.pwinputG').value}`)
+    ///
+
+
+    } 
+
+    if (!this.state.username) {
+      return this.setState({ error: 'Username is required' });
+    }
+
+    if (!this.state.password) {
+      return this.setState({ error: 'Password is required' });
+    }
+
+    return this.setState({ error: '' });
+  }
+
+    
+
+  handleUserChange(evt) {
+    this.setState({
+      username: evt.target.value,
+    });
+  };
+
+  handlePassChange(evt) {
+    this.setState({
+      password: evt.target.value,
+    });
+  }
+
+
+
+  
+  render() {
+
+    return (
+      
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          {
+            this.state.error &&
+            <h3 data-test="error" onClick={this.dismissError}>
+              <button onClick={this.dismissError}>✖</button>
+              {this.state.error}
+            </h3>
+          }
+
+          <img src={logo} className="background" alt="Logo" />
+          <div className="windowUI"></div>
+
+
+
+          <label className="UserNameG">User Name</label>
+          <input type="text" className="userNameInputG" data-test="username" value={this.state.username} onChange={this.handleUserChange} />
+          <div className="Background"></div>
+          <label className="PasswordG">Password</label>
+          <input type="password"className="pwinputG" data-test="password" value={this.state.password} onChange={this.handlePassChange} />
+
+          <input type="submit" className="signInG" value="Sign In" data-test="submit" />
+
+          <input type="submit" className="signUpG" value="Sign Up" data-test="submit" />
+
+          <a href="https://google.com" className="forgotPW">Forgot Password?</a>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React  from 'react';
 import Menu from './Menu';
 import ListOfRoomReviews from './listOfRoomReviews';
+import businessLogo from './businessLogo.png';
 
 const UserProfile = () => {
     return (  <div>
@@ -12,4 +13,187 @@ const UserProfile = () => {
     )
 };
 
-export default UserProfile
+class userProfile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+      email:'JanekKowalkski@s.pl',
+      firstname:'Janek',
+      secondname:'Kowalski',
+      error: '',
+    };
+  }
+
+
+
+  dismissError() {
+    this.setState({ error: '' });
+  }
+
+  phone(evt) {
+    evt.preventDefault();
+  console.log('s')
+  }
+
+  myReservation(evt) {
+    evt.preventDefault();
+  console.log('s')
+  }
+  myRequests(evt) {
+    evt.preventDefault();
+  console.log('s')
+  }
+
+  profile(evt) {
+    evt.preventDefault();
+  console.log('s')
+  }
+
+  email(evt) {
+    evt.preventDefault();
+  console.log('s')
+  }
+
+  logOut(evt) {
+    evt.preventDefault();
+  console.log('s')
+  }
+
+  addNewOffer(evt) {
+    evt.preventDefault();
+  console.log('s')
+  }
+
+  seeYourRoomRequests(evt) {
+    evt.preventDefault();
+  console.log('s')
+  }
+
+
+
+  
+  render() {
+
+    return (
+      
+      <div className="container">
+
+        <div className="navigation">
+         <div className="businessLogo">
+         <img src={businessLogo} className="businessLogo" alt="businessLogo" />
+         </div>
+          <div className="Frame4"></div>
+          <div  className="Phone" onClick={this.phone}>Room Offers</div>
+          <div className="myReservations" onClick={this.myReservation}>My Reservations</div>
+          <div className="myRequests" onClick={this.myRequests}>My Requests</div>
+          <div className="Profile" onClick={this.profile}>Profile</div>
+
+           <div className="userLogo">
+            {/* //img src */}
+           </div>
+           <div className="email" onClick={this.email}>{this.state.email}</div>
+           <div className="logOut" onClick={this.logOut}>LOGOUT</div>    
+           </div>
+
+      <div className="body">
+           <div className="mainLogo">  {/* //img src */} </div>
+            <div className='userFirstandSecondname'>{this.state.firstname} {this.state.secondname}</div>
+
+                     <div className='table'>
+                     <table>
+                      <tr>
+                        <th>username</th>
+                        <th>  email</th>
+                      </tr>
+                      <tr>
+                        <td>{this.state.firstname} {this.state.secondname}</td>
+                        <td>{this.state.email}</td>
+                      </tr>
+                      </table>
+                      </div>
+          <div className="text">Your offer following rooms as a host</div>
+          <button className="addNewOffer" value="Add new offer" onClick={this.addNewOffer}>ADD NEW OFFER</button>
+          <button className="seeYourRoomRequests" value="See Your Room Request" onClick={this.seeYourRoomRequests}>SEE YOUR ROOM REQUESTS</button>
+           </div>
+
+
+      <div className="roomOffers"></div>
+         <div className="window1">
+
+            <div className="inwindow1userimg">
+            {/* {//img src*/ }
+            br  USER 
+            br  IMG
+            </div>
+
+            <div className="inwindow1title">title</div>
+            <div className="inwindow1location">roomLocation</div>
+            <div className="inwindow1roomimage">
+            {/* {//img src*/ }
+           imgsrc
+            </div>
+            <div className="inwindow1description">description</div>
+            <div className="inwindow1guestnumber">Number of guests: numberofGuests</div>
+            <div className="inwindow1price">Price: price </div>
+            <div className="inwindow1avaiblereservation">Avaible to make reservation: </div>
+         </div>
+ 
+ 
+         <div className="window2">
+
+         <div className="inwindow1userimg">
+            {/* {//img src*/ }
+            br  USER 
+            br  IMG
+            </div>
+
+            <div className="inwindow1title">title</div>
+            <div className="inwindow1location">roomLocation</div>
+            <div className="inwindow1roomimage">
+            {/* {//img src*/ }
+           imgsrc
+            </div>
+            <div className="inwindow1description">description</div>
+            <div className="inwindow1guestnumber">Number of guests: numberofGuests</div>
+            <div className="inwindow1price">Price: price </div>
+            <div className="inwindow1avaiblereservation">Avaible to make reservation: </div>
+           </div>
+
+
+           
+           <div className="window3">
+           
+           <div className="inwindow1userimg">
+            {/* {//img src*/ }
+            br  USER 
+            br  IMG
+            </div>
+
+            <div className="inwindow1title">title</div>
+            <div className="inwindow1location">roomLocation</div>
+            <div className="inwindow1roomimage">
+            {/* {//img src*/ }
+           imgsrc
+            </div>
+            <div className="inwindow1description">description</div>
+            <div className="inwindow1guestnumber">Number of guests: numberofGuests</div>
+            <div className="inwindow1price">Price: price </div>
+            <div className="inwindow1avaiblereservation">Avaible to make reservation: </div>
+           </div>
+
+      </div>
+
+
+
+
+  
+   
+
+
+    );
+  }
+}
+
+export default userProfile;
